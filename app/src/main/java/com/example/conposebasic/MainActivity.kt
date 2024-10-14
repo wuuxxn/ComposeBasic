c535f7cea97f356a13153cab89e139eeeadb7309
wuuxxn <owen20050723@gmail.com>
2024-10-14 16:56:41 +0800

    第2次

--- app/src/main/java/com/example/conposebasic/MainActivity.kt
+++ app/src/main/java/com/example/conposebasic/MainActivity.kt
@@ -5,15 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,6 +28,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.conposebasic.ui.theme.ConposeBasicTheme
 
@@ -45,42 +51,67 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    //var x by remember { mutableStateOf(0) }
+    var x = remember { mutableStateOf(0) }
     Column {
+        Row {
+            Button(onClick = {
+                x.value = 0
+            }) {
+                Text("歸零")
+                Image(
+                    painter = painterResource(id = R.drawable.aaa),
+                    contentDescription = "存錢吧",
+                    modifier = modifier.size(40.dp)
+                )
+            }
+            Button(onClick = {
+                x.value = x.value * 2
+            }) {
+                Text("*2")
+                Image(
+                    painter = painterResource(id = R.drawable.aaa),
+                    contentDescription = "存錢吧",
+                    modifier = modifier.size(40.dp)
+                )
+            }
+        }
+        /*Text(text =x.toString(), 用by的寫法
+            fontSize =50.sp,
+            modifier = modifier.clickable { x++ },)*/
+        Text(text =x.value.toString(),
+            fontSize =50.sp,
+            modifier = modifier.clickable { x.value++ },)
+
         Row {
             Text(
                 text = stringResource(R.string.author),
                 color = Color.Blue,
                 fontSize = 30.sp,
                 fontFamily = FontFamily(Font(R.font.hand)),
-                modifier = modifier
-            )
+
+                )
             Image(
-
-                painter = painterResource(id = R.drawable.tcyang1),
-                contentDescription = "子青快樂旅遊照",
-                alpha = 0.8f,
-                modifier = modifier.clip(CircleShape)
-
-            )
-        }
-        Box(
-            modifier = Modifier.fillMaxSize(),
-
-            contentAlignment = Alignment.Center
-
-
-        ){
-            Image(
-
                 painter = painterResource(id = R.drawable.tcyang1),
                 contentDescription = "子青快樂旅遊照",
                 alpha = 0.8f,
                 modifier = modifier.clip(CircleShape),
             )
-
+        }
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.tcyang1),
+                contentDescription = "子青快樂",
+                alpha = 0.8f,
+                modifier = modifier.clip(CircleShape),
+            )
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
